2709f73ca6c4f183e19a9866df6e0795af59126b
jihoon <ljhljh211@naver.com>
2022-10-04 17:29:13 +0900

    [MOD] NEWS FEED API 데이터 가져오기

--- src/pages/Board/Notice/NoticeList.jsx
+++ src/pages/Board/Notice/NoticeList.jsx
@@ -1,11 +1,27 @@
 import HeaderContainer from "../../../components/HeaderContainer"
 
+/* 
+axios 활용해서 처리해보기 -> 추후 과제
+- fetch 개념 이해하기
+
+*/
+const ajax = new XMLHttpRequest();
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json" 
+ajax.open(
+  'GET', // GET 방식
+  NEWS_URL,
+  false // 동기적으로 처리하겠다.
+);
+ajax.send(); // 데이터를 가져옴
+const newsFeed = JSON.parse(ajax.response);
+
+console.log(newsFeed);
+
 
 export default function NoticeList() {
   return (
     <>
       <HeaderContainer />
-
       <div>게시판 리스트</div>
       
     </>
