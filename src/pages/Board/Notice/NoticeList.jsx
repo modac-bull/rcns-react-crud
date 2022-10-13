2c3085026cc6bda6ba8098d89e162e2f292565b8
leejihoon <ljhljh211@naver.com>
2022-10-13 09:19:58 +0900

    [MOD] 노액트-CRUD 목록 state 활용하여 목록 표시, 페이지네이션 기능 추가

--- src/pages/Board/Notice/NoticeList.jsx
+++ src/pages/Board/Notice/NoticeList.jsx
@@ -1,6 +1,8 @@
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 
+import {useState, useEffect} from 'react';
+
 import {Link} from 'react-router-dom'
 
 
@@ -13,32 +15,36 @@ import FooterContainer from '../../../components/FooterContainer';
 axios 활용해서 처리해보기 -> 추후 과제
 - fetch 개념 이해하기
 
+// fetch('https://codingapple1.github.io/shop/data2.json')
+// .then(result => result.json())
+// .then(data=> {
+//   console.log(data, 'fetch')
+// })
+
 */
-const ajax = new XMLHttpRequest();
-const MAX_PAGINATION = 10;
-let CURRENT_PAGE = 1;
+const MAX_PAGINATION = 10; // 최대 페이지네이션 숫자
+let CURRENT_PAGE = 1; // 초기 데이터
 const NEWS_URL = `https://api.hnpwa.com/v0/news/${CURRENT_PAGE}.json`
-ajax.open(
-  'GET', // GET 방식
-  NEWS_URL,
-  false // 동기적으로 처리하겠다.
-);
-ajax.send(); // 데이터를 가져옴
-const newsFeed = JSON.parse(ajax.response); // 
-
-console.log(newsFeed);
 
 
 
-/* 
-클릭했을 때 내용이 변경되게 하기 위해서 필요한 개념
--> HOOK? 
-*/
-function handleClick(e, index) {
-  e.preventDefault();
-}
 
 export default function NoticeList() {
+  const [list, setList] = useState([]); // 게시판 데이터
+  const [index, setIndex] = useState(CURRENT_PAGE); // 데이터 
+  
+  useEffect(() => { // mount시 1회 실행
+    console.log('useEffect')
+
+    fetch(NEWS_URL)
+    .then(result => result.json())
+    .then(data => {
+      console.log(data)
+      setList(data) // 게시판 1번째 불러온 데이터로 렌더
+    })
+
+    
+  }, [])
   /* 
     1. return 문 안에 for문 사용할 수 없어 함수를 만들어 return문에 반복된 jsx 배열이 포함되도록 하였음
     2. 이렇게 될 경우 함수 생성 위치는 어디가 좋을까?
@@ -47,12 +53,28 @@ export default function NoticeList() {
     const result  = [];
     for(let i = 0; i < MAX_PAGINATION ; i++) {
       result.push(
-        CURRENT_PAGE === i+1 ? 
-          <span key={i+1} className={"ml-5 mr-5 font-bold"} onClick={(e) => handleClick(e, i)}>{i+1}</span>
+        index === i+1 ? // 현재 인덱스 일치할 경우 bold
+          <span key={i+1} className={"ml-5 mr-5 font-bold cursor-pointer"} onClick={(e) => handleClick(e, i)}>{i+1}</span>
         : 
-          <span key={i+1} className={"ml-5 mr-5 text-gray-400"} onClick={(e) => handleClick(e, i)}>{i+1}</span>
+          <span key={i+1} className={"ml-5 mr-5 text-gray-400 cursor-pointer"} onClick={(e) => handleClick(e, i)}>{i+1}</span>
       )
     }
+
+    /* 
+    클릭했을 때 내용이 변경되게 하기 위해서 필요한 개념
+    -> HOOK? 
+    */
+    function handleClick(e, index) {
+      setIndex(index+1); // 페이지네이션
+      e.preventDefault();
+      fetch(`https://api.hnpwa.com/v0/news/${index+1}.json`)
+      .then(result => result.json())
+      .then(data=> {
+        // console.log(data, 'fetch');
+        setList(data); 
+      })
+    }
+
     return (
       <div className="mt-10 text-center">
         {result}
@@ -67,14 +89,15 @@ export default function NoticeList() {
     <>
       <HeaderContainer />
       <PageWrap>
-        <h1 className="text-3xl text-center font-bold mb-10 ">공지사항 게시판</h1>
+        <h1 className="text-3xl text-center font-bold mb-10 ">게시판 실습</h1>
         <List className="bg-gray">
-          { (newsFeed.length === 0) ? (
+          { (list.length === 0) ? (
             <p className="text-l text-center mt-12 mb-12 font-bold">게시물이 없습니다.</p>
           ) : (
-            newsFeed.map((list) => (
-              NoticeItem(list)
-            ))
+            list.map((props) => {
+              {/* return NoticeItem(props) // 이렇게 할 경우 함수컴포넌트에서 .변수명 으로 한번 더 타고 들어갈 필요는 없는것 같다. */}
+              return <NoticeItem key={props.id}  list={props}></NoticeItem>
+            })
           )}
         </List>
         {/* 페이지네이션 */}
@@ -85,22 +108,23 @@ export default function NoticeList() {
   ) 
 }
 
-function NoticeItem(list) {
-  console.log(list.type);
+function NoticeItem(props) {
+  // console.log(props)
+  // console.log(props.type);
 
   function timeToDate(date) {
     return (new Date(date * 1000)).toUTCString();
   }
 
   return(
-    <Item key={list.id} className="py-4 flex">
+    <Item className="py-4 flex">
       <LinkItem
-        to='/notice/details/'
+        to={'/notice/details/'+props.list.id}
       >
         <div className="ml-3">
-          <p className="text-m font-medium text-gray-900">{list.title}</p>
-          <p className="text-sm text-gray-500">{list.user}</p>
-          <p className="text-xs text-gray-300">{timeToDate(list.time)}</p>
+          <p className="text-m font-medium text-gray-900">{props.list.title}</p>
+          <p className="text-sm text-gray-500">{props.list.user}</p>
+          <p className="text-xs text-gray-300">{timeToDate(props.list.time)}</p>
         </div>
       </LinkItem>
     </Item>
